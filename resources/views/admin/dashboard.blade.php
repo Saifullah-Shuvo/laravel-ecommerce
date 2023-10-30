32d96f7af7d4191b64e8c1c2c4b5fbfb53529e80
Saifullah-Shuvo <shuvo70544@gmail.com>
2023-10-30 11:14:25 +0600

    Admin Dashboard update

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -272,7 +272,7 @@
                                             <td>
                                                 <div class="d-flex align-items-center">
                                                     <div class="avatar-sm bg-light rounded p-1 me-2">
-                                                        <img src="assets/images/products/img-1.png" alt="" class="img-fluid d-block" />
+                                                        <img src="{{asset('admins')}}/assets/images/products/img-1.png" alt="" class="img-fluid d-block" />
                                                     </div>
                                                     <div>
                                                         <h5 class="fs-14 my-1"><a href="apps-ecommerce-product-details.html" class="text-reset">Branded T-Shirts</a></h5>
@@ -301,7 +301,7 @@
                                             <td>
                                                 <div class="d-flex align-items-center">
                                                     <div class="avatar-sm bg-light rounded p-1 me-2">
-                                                        <img src="assets/images/products/img-2.png" alt="" class="img-fluid d-block" />
+                                                        <img src="{{asset('admins')}}/assets/images/products/img-2.png" alt="" class="img-fluid d-block" />
                                                     </div>
                                                     <div>
                                                         <h5 class="fs-14 my-1"><a href="apps-ecommerce-product-details.html" class="text-reset">Bentwood Chair</a></h5>
@@ -330,7 +330,7 @@
                                             <td>
                                                 <div class="d-flex align-items-center">
                                                     <div class="avatar-sm bg-light rounded p-1 me-2">
-                                                        <img src="assets/images/products/img-3.png" alt="" class="img-fluid d-block" />
+                                                        <img src="{{asset('admins')}}/assets/images/products/img-3.png" alt="" class="img-fluid d-block" />
                                                     </div>
                                                     <div>
                                                         <h5 class="fs-14 my-1"><a href="apps-ecommerce-product-details.html" class="text-reset">Borosil Paper Cup</a></h5>
@@ -359,7 +359,7 @@
                                             <td>
                                                 <div class="d-flex align-items-center">
                                                     <div class="avatar-sm bg-light rounded p-1 me-2">
-                                                        <img src="assets/images/products/img-4.png" alt="" class="img-fluid d-block" />
+                                                        <img src="{{asset('admins')}}/assets/images/products/img-4.png" alt="" class="img-fluid d-block" />
                                                     </div>
                                                     <div>
                                                         <h5 class="fs-14 my-1"><a href="apps-ecommerce-product-details.html" class="text-reset">One Seater Sofa</a></h5>
@@ -388,7 +388,7 @@
                                             <td>
                                                 <div class="d-flex align-items-center">
                                                     <div class="avatar-sm bg-light rounded p-1 me-2">
-                                                        <img src="assets/images/products/img-5.png" alt="" class="img-fluid d-block" />
+                                                        <img src="{{asset('admins')}}/assets/images/products/img-5.png" alt="" class="img-fluid d-block" />
                                                     </div>
                                                     <div>
                                                         <h5 class="fs-14 my-1"><a href="apps-ecommerce-product-details.html" class="text-reset">Stillbird Helmet</a></h5>
@@ -474,7 +474,7 @@
                                             <td>
                                                 <div class="d-flex align-items-center">
                                                     <div class="flex-shrink-0 me-2">
-                                                        <img src="assets/images/companies/img-1.png" alt="" class="avatar-sm p-2" />
+                                                        <img src="{{asset('admins')}}/assets/images/companies/img-1.png" alt="" class="avatar-sm p-2" />
                                                     </div>
                                                     <div>
                                                         <h5 class="fs-14 my-1 fw-medium">
@@ -502,7 +502,7 @@
                                             <td>
                                                 <div class="d-flex align-items-center">
                                                     <div class="flex-shrink-0 me-2">
-                                                        <img src="assets/images/companies/img-2.png" alt="" class="avatar-sm p-2" />
+                                                        <img src="{{asset('admins')}}/assets/images/companies/img-2.png" alt="" class="avatar-sm p-2" />
                                                     </div>
                                                     <div class="flex-grow-1">
                                                         <h5 class="fs-14 my-1 fw-medium"><a href="apps-ecommerce-seller-details.html" class="text-reset">Digitech Galaxy</a></h5>
@@ -528,7 +528,7 @@
                                             <td>
                                                 <div class="d-flex align-items-center">
                                                     <div class="flex-shrink-0 me-2">
-                                                        <img src="assets/images/companies/img-3.png" alt="" class="avatar-sm p-2" />
+                                                        <img src="{{asset('admins')}}/assets/images/companies/img-3.png" alt="" class="avatar-sm p-2" />
                                                     </div>
                                                     <div class="flex-gow-1">
                                                         <h5 class="fs-14 my-1 fw-medium"><a href="apps-ecommerce-seller-details.html" class="text-reset">Nesta Technologies</a></h5>
@@ -554,7 +554,7 @@
                                             <td>
                                                 <div class="d-flex align-items-center">
                                                     <div class="flex-shrink-0 me-2">
-                                                        <img src="assets/images/companies/img-8.png" alt="" class="avatar-sm p-2" />
+                                                        <img src="{{asset('admins')}}/assets/images/companies/img-8.png" alt="" class="avatar-sm p-2" />
                                                     </div>
                                                     <div class="flex-grow-1">
                                                         <h5 class="fs-14 my-1 fw-medium"><a href="apps-ecommerce-seller-details.html" class="text-reset">Zoetic Fashion</a></h5>
@@ -580,7 +580,7 @@
                                             <td>
                                                 <div class="d-flex align-items-center">
                                                     <div class="flex-shrink-0 me-2">
-                                                        <img src="assets/images/companies/img-5.png" alt="" class="avatar-sm p-2" />
+                                                        <img src="{{asset('admins')}}/assets/images/companies/img-5.png" alt="" class="avatar-sm p-2" />
                                                     </div>
                                                     <div class="flex-grow-1">
                                                         <h5 class="fs-14 my-1 fw-medium">
@@ -700,7 +700,7 @@
                                             <td>
                                                 <div class="d-flex align-items-center">
                                                     <div class="flex-shrink-0 me-2">
-                                                        <img src="assets/images/users/avatar-1.jpg" alt="" class="avatar-xs rounded-circle shadow" />
+                                                        <img src="{{asset('admins')}}/assets/images/users/avatar-1.jpg" alt="" class="avatar-xs rounded-circle shadow" />
                                                     </div>
                                                     <div class="flex-grow-1">Alex Smith</div>
                                                 </div>
@@ -724,7 +724,7 @@
                                             <td>
                                                 <div class="d-flex align-items-center">
                                                     <div class="flex-shrink-0 me-2">
-                                                        <img src="assets/images/users/avatar-2.jpg" alt="" class="avatar-xs rounded-circle shadow" />
+                                                        <img src="{{asset('admins')}}/assets/images/users/avatar-2.jpg" alt="" class="avatar-xs rounded-circle shadow" />
                                                     </div>
                                                     <div class="flex-grow-1">Jansh Brown</div>
                                                 </div>
@@ -748,7 +748,7 @@
                                             <td>
                                                 <div class="d-flex align-items-center">
                                                     <div class="flex-shrink-0 me-2">
-                                                        <img src="assets/images/users/avatar-3.jpg" alt="" class="avatar-xs rounded-circle shadow" />
+                                                        <img src="{{asset('admins')}}/assets/images/users/avatar-3.jpg" alt="" class="avatar-xs rounded-circle shadow" />
                                                     </div>
                                                     <div class="flex-grow-1">Ayaan Bowen</div>
                                                 </div>
@@ -772,7 +772,7 @@
                                             <td>
                                                 <div class="d-flex align-items-center">
                                                     <div class="flex-shrink-0 me-2">
-                                                        <img src="assets/images/users/avatar-4.jpg" alt="" class="avatar-xs rounded-circle shadow" />
+                                                        <img src="{{asset('admins')}}/assets/images/users/avatar-4.jpg" alt="" class="avatar-xs rounded-circle shadow" />
                                                     </div>
                                                     <div class="flex-grow-1">Prezy Mark</div>
                                                 </div>
@@ -796,7 +796,7 @@
                                             <td>
                                                 <div class="d-flex align-items-center">
                                                     <div class="flex-shrink-0 me-2">
-                                                        <img src="assets/images/users/avatar-6.jpg" alt="" class="avatar-xs rounded-circle shadow" />
+                                                        <img src="{{asset('admins')}}/assets/images/users/avatar-6.jpg" alt="" class="avatar-xs rounded-circle shadow" />
                                                     </div>
                                                     <div class="flex-grow-1">Vihan Hudda</div>
                                                 </div>
