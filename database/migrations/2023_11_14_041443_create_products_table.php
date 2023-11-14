0b3954d3d01e2d9c935196efa2cff4741d9addac
Saifullah-Shuvo <shuvo70544@gmail.com>
2023-11-14 11:16:25 +0600

    product table migration table

---
+++ database/migrations/2023_11_14_041443_create_products_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('category_id');
+            $table->string('name');
+            $table->string('code');
+            $table->string('unit')->nullable();
+            $table->string('tags')->nullable();
+            $table->string('purchase_price')->nullable();
+            $table->string('selling_price')->nullable();
+            $table->string('discount_price')->nullable();
+            $table->string('stock_quantity')->nullable();
+            $table->string('description')->nullable();
+            $table->string('thambnail')->nullable();
+            $table->string('images')->nullable();
+            $table->integer('featured')->nullable();
+            $table->integer('hot_deal')->nullable();
+            $table->integer('popular_product')->nullable();
+            $table->integer('status')->nullable();
+            $table->timestamps();
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
