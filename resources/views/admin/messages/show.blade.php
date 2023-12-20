1a4e715e8988161bb435ea3de87f261dc255a035
Saifullah-Shuvo <shuvo70544@gmail.com>
2023-12-20 09:52:52 +0600

    Admin message show

--- resources/views/admin/messages/show.blade.php
+++ resources/views/admin/messages/show.blade.php
@@ -16,123 +16,63 @@
             </ol>
         </div>
     </div>
-    @foreach($details as $data)
-    <form action="{{ route('details.update',['id'=>$data->id]) }}" method="POST" enctype="multipart/form-data" id="image-upload">
+
+    <form action="" method="POST" enctype="multipart/form-data" id="image-upload">
         @csrf
         <div class="row justify-content-center">
             <div class="col-lg-10">
                 <div class="card">
                     <div class="card-body">
                         <div class="mb-3">
-                            <label class="form-label" for="product-title-input">Address</label>
-                            <input type="text" name="adderess" class="form-control" id="product-title-input" value="{{ $data->adderess }}">
-                            @error('adderess')
+                            <label class="form-label" for="product-title-input">Name</label>
+                            <input type="text" name="name" class="form-control" id="product-title-input" value="{{ $message->name }}">
+                            @error('name')
                                 <div class="error"><span class="text-danger">{{ $message }}</span></div>
                             @enderror
                         </div>
                         <div class="mb-3">
-                            <label class="form-label" for="product-title-input">Main Email</label>
-                            <input type="email" name="main_email" class="form-control" id="product-title-input" value="{{ $data->main_email }}">
-                            @error('main_email')
+                            <label class="form-label" for="product-title-input">Email</label>
+                            <input type="email" name="email" class="form-control" id="product-title-input" value="{{ $message->email }}">
+                            @error('email')
                                 <div class="error"><span class="text-danger">{{ $message }}</span></div>
                             @enderror
                         </div>
+
+
                         <div class="mb-3">
-                            <label class="form-label" for="product-title-input">Alternative Email</label>
-                            <input type="email" name="alternative_email" class="form-control" id="product-title-input" value="{{ $data->alternative_email }}">
-                            @error('alternative_email')
+                            <label class="form-label" for="product-title-input">Subject</label>
+                            <input type="text" name="subject" class="form-control" id="product-title-input" value="{{ $message->subject }}">
+                            @error('subject')
                                 <div class="error"><span class="text-danger">{{ $message }}</span></div>
                             @enderror
                         </div>
-                        <div class="mb-3">
-                            <label class="form-label" for="product-title-input">Main Phone</label>
-                            <input type="number" name="main_phone" class="form-control" id="product-title-input" value="{{ $data->main_phone }}">
-                            @error('main_phone')
-                                <div class="error"><span class="text-danger">{{ $message }}</span></div>
-                            @enderror
-                        </div>
-                        <div class="mb-3">
-                            <label class="form-label" for="product-title-input">Alternate Phone</label>
-                            <input type="number" name="alternative_phone" class="form-control" id="product-title-input" value="{{ $data->alternative_phone }}">
-                            @error('alternative_phone')
-                                <div class="error"><span class="text-danger">{{ $message }}</span></div>
-                            @enderror
-                        </div>
-                        <div class="mb-3">
-                            <label class="form-label" for="product-title-input">Facebook</label>
-                            <input type="text" name="facebook" class="form-control" id="product-title-input" value="{{ $data->facebook }}">
-                            @error('facebook')
-                                <div class="error"><span class="text-danger">{{ $message }}</span></div>
-                            @enderror
-                        </div>
-                        <div class="mb-3">
-                            <label class="form-label" for="product-title-input">Tweeter</label>
-                            <input type="text" name="tweeter" class="form-control" id="product-title-input" value="{{ $data->tweeter }}">
-                            @error('tweeter')
-                                <div class="error"><span class="text-danger">{{ $message }}</span></div>
-                            @enderror
-                        </div>
-                        <div class="mb-3">
-                            <label class="form-label" for="product-title-input">Linked In</label>
-                            <input type="text" name="linkedIn" class="form-control" id="product-title-input" value="{{ $data->linkedIn }}">
-                            @error('linkedIn')
-                                <div class="error"><span class="text-danger">{{ $message }}</span></div>
-                            @enderror
-                        </div>
-                        <div class="mb-3">
-                            <label class="form-label" for="product-title-input">Google Plus</label>
-                            <input type="text" name="google_plus" class="form-control" id="product-title-input" value="{{ $data->google_plus }}">
-                            @error('google_plus')
-                                <div class="error"><span class="text-danger">{{ $message }}</span></div>
-                            @enderror
-                        </div>
+
                         <div>
-                            <label>Short Details</label>
+                            <label>Message</label>
 
                             <div id="ckeditor-classic">
                                 <div>
-                                    <textarea name="short_details" class="form-control" id="exampleFormControlTextarea5" rows="5"
-                                    placeholder="Enter Review Text">{{ $data->short_details }}</textarea>
+                                    <textarea name="message" class="form-control" id="exampleFormControlTextarea5" rows="5"
+                                    placeholder="Enter Review Text">{{ $message->message }}</textarea>
                                 </div>
-                                @error('short_details')
+                                @error('message')
                                     <div class="error mt-1"><span class="text-danger">{{ $message }}</span></div>
                                 @enderror
                             </div>
-                        </div>
-                        <div>
-                            <label>About Details</label>
-
-                            <div id="ckeditor-classic">
-                                <div>
-                                    <textarea name="about_details" class="form-control" id="exampleFormControlTextarea5" rows="10"
-                                    placeholder="Enter Review Text">{{ $data->about_details }}</textarea>
-                                </div>
-                                @error('about_details')
-                                    <div class="error mt-1"><span class="text-danger">{{ $message }}</span></div>
-                                @enderror
-                            </div>
-                        </div>
-                        <div class="mb-3">
-                            <label class="form-label" for="product-title-input">Google Map (Iframe link)</label>
-                            <input type="text" name="google_map" class="form-control" id="product-title-input" value="{{ $data->google_map }}">
-                            @error('google_map')
-                                <div class="error"><span class="text-danger">{{ $message }}</span></div>
-                            @enderror
                         </div>
                     </div>
                 </div>
                 <!-- end card -->
 
-                <div class="text-start mb-3">
+                {{-- <div class="text-start mb-3">
                     <button type="submit" class="btn btn-success w-lg ml-3">Update</button>
-                </div>
+                </div> --}}
             </div>
             <!-- end col -->
         </div>
         <!-- end row -->
 
     </form>
-    @endforeach
 @endsection
 
 
